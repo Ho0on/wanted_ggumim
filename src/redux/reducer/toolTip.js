9d921caea879c31dd0a1fa7874e15921fee24e08
SangHoon <joa3668@gmail.com>
2022-05-05 17:17:52 +0900

    ♻️ [Refactor] change switch

--- src/redux/reducer/toolTip.js
+++ src/redux/reducer/toolTip.js
@@ -3,10 +3,11 @@ import { SELECT_ITEM } from '../actions/types';
 const initialState = null;
 
 const reducer = (previousState = initialState, action) => {
-  if (action.type === SELECT_ITEM) {
-    return action.itemId;
-  } else {
-    return previousState;
+  switch (action.type) {
+    case SELECT_ITEM:
+      return action.itemId;
+    default:
+      return previousState;
   }
 };
 
